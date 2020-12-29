eb16f400df34f2909a31694778b04db64cc82312
cotgyu <v123v123s@naver.com>
2020-12-29 13:07:33 +0900

    chap 3 - JPA 사용

---
+++ src/main/java/com/cotgyu/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.cotgyu.book.springboot.web.dto;
+
+import com.cotgyu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
+
